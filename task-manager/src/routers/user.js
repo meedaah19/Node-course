91b30432078d97e84a4ae9b95200f0dd82825337
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-10-29 11:42:54 +0100

    feat: update user model and router to enhance user data handling and remove unused routes

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -51,19 +51,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-       const user = await User.findById(_id)
-       if(!user) {
-        return res.status(404).send()
-       }
-       res.status(201).send(user)
-    } catch (error) {
-        res.send(500).send(error)
-    }
-})
-
 router.patch('/users/:id', async(req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -80,7 +67,6 @@ router.patch('/users/:id', async(req, res) => {
 
         await user.save()
 
-        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
         if(!user) {
             return res.status(404).send()
         }
@@ -91,15 +77,10 @@ router.patch('/users/:id', async(req, res) => {
     }
 })
 
-router.delete('user/:id', async(req, res) => {
+router.delete('/users/delete', auth, async(req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
-
-        if(!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user);
+        await req.user.deleteOne()
+        res.send(req.user);
     } catch (error) {
         res.status(500).send()
     }
