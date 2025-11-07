137c473152caa5b74bf1c444951a0f9b1af8ec03
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-11-07 05:26:15 +0100

    feat: add avatar upload and retrieval functionality with sharp image processing

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,10 +6,6 @@ import {routerTask } from './routers/task.js'
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//    res.status(503).send('This app is under maintainace, pls bare with us and come back later');
-    
-// })
 
 app.use(express.json());
 app.use(router);
@@ -19,17 +15,3 @@ app.use(routerTask );
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-// import Tasks from './models/task.js';
-// import User from './models/user.js';
-
-// const main = async () => {
-//     // const task = await Tasks.findById('648b1f4f4f1a2565f4e2b6d3');
-//     // await task.populate('owner');
-//     // console.log(task.owner);
-//     const user = await User.findById('6905075aebfb9fa1426b6eab');
-//     await user.populate('tasks');
-//     console.log(user.tasks);
-// }
-
-// main();
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ import User from '../models/user.js';
 import express from 'express';
 import auth from '../middlewares/auth.js'
 import multer from 'multer'
+import sharp from 'sharp'                                 
 
 const router = new express.Router()
 
@@ -95,11 +96,37 @@ router.delete('/users/delete', auth, async(req, res) => {
     }
 });
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
-    
+router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer;
+    await req.user.save()
     res.send() 
 } , (error, req, res, next) => {
     res.status(400).send({error: error.message})
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) =>{
+    try {
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send()
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
 })
 
 export {router};
